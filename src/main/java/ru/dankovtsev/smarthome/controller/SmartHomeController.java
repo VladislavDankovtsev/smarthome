28d5125abd1a5db263dbdcd72dc3aa78487b45ef
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-04-02 14:37:57 +0300

    проверка соединения с главным сервером

--- src/main/java/ru/dankovtsev/smarthome/controller/SmartHomeController.java
+++ src/main/java/ru/dankovtsev/smarthome/controller/SmartHomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ru.dankovtsev.smarthome.model.CheckConnection;
 import ru.dankovtsev.smarthome.model.SmartHome;
 import ru.dankovtsev.smarthome.service.SmartHomeService;
 import ru.dankovtsev.smarthome.service.SmartHomeServiceConfig;
@@ -18,5 +19,11 @@ public class SmartHomeController {
     @RequestMapping(path = "/online", method = RequestMethod.GET)
     public SmartHome getSmartHome(){
         return smartHomeServiceConfig.getSmartHome();
+    }
+    @RequestMapping(path = "/check", method = RequestMethod.GET)
+    public CheckConnection getAgricultureConnection(){
+        CheckConnection checkConnection = new CheckConnection();
+        checkConnection.setCheck("work");
+        return checkConnection;
     }
 }
